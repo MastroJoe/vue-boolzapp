6cae53250a1e9f6f8edf39f74e241c42406e4b91
Giorgio <giorgio.pignattari@gmail.it>
2021-03-26 15:57:10 +0100

    inizio html

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Descrizione
+// Riprodurre una poco conosciuta web app di messaggistica.
+
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+  }
+);
