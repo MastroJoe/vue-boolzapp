e8a2806bd92c81a069a5c8c549ad73d4d4be7164
Giorgio <giorgio.pignattari@gmail.it>
2021-03-28 23:21:19 +0200

    parte chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,7 +74,7 @@ var app = new Vue(
       	},
       	{
       		name: 'Luisa',
-      		avatar: '_4',
+      		avatar: '_6',
       		visible: true,
       		messages: [
       			{
@@ -89,6 +89,94 @@ var app = new Vue(
       			}
       		],
       	},
+        {
+          name: 'Marco',
+          avatar: '_8',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Fabio',
+          avatar: '_7',
+          visible: true,
+          messages: [
+            {
+              date: '20/03/2020 16:30:00',
+              text: 'Ciao come stai?',
+              status: 'sent'
+            },
+            {
+              date: '20/03/2020 16:30:55',
+              text: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
+            },
+            {
+              date: '20/03/2020 16:35:00',
+              text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'sent'
+            }
+          ],
+        },
+        {
+          name: 'Alessandro',
+          avatar: '_5',
+          visible: true,
+          messages: [
+            {
+              date: '20/03/2020 16:30:00',
+              text: 'Ciao come stai?',
+              status: 'sent'
+            },
+            {
+              date: '20/03/2020 16:30:55',
+              text: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
+            },
+            {
+              date: '20/03/2020 16:35:00',
+              text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'sent'
+            }
+          ],
+        },
+        {
+          name: 'Giorgio',
+          avatar: '_4',
+          visible: true,
+          messages: [
+            {
+              date: '20/03/2020 16:30:00',
+              text: 'Ciao come stai?',
+              status: 'sent'
+            },
+            {
+              date: '20/03/2020 16:30:55',
+              text: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
+            },
+            {
+              date: '20/03/2020 16:35:00',
+              text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'sent'
+            }
+          ],
+        }
       ]
     },
     computed: {
